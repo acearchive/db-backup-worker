900f6ed162d0740973a12fba5a5a076b386c9a29
Frawley <frawley@duck.com>
2025-02-09 00:55:09 -0500

    Add workers secrets

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 use worker::{self, event, Env, ScheduleContext, ScheduledEvent};
 
+const CLOUDFLARE_ACCOUNT_ID_VAR: &str = "ACCOUNT_ID";
+const CLOUDFLARE_API_TOKEN_VAR: &str = "API_TOKEN";
+const DB_ID_VAR: &str = "DB_ID";
+const R2_BINDING: &str = "R2";
+
 #[event(scheduled)]
 async fn scheduled(_event: ScheduledEvent, env: Env, _ctx: ScheduleContext) {
     console_error_panic_hook::set_once();
